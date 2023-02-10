e895edff628bf7fef7f2cad36b1f63e28f966021
linh128 <112847909+linh1298@users.noreply.github.com>
2023-02-10 19:27:49 +0700

    DrinkListActivity
    recyclerview

---
+++ app/src/main/java/com/example/mikky/activities/DrinkListActivity.java
@@ -0,0 +1,42 @@
+package com.example.mikky.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.mikky.R;
+import com.example.mikky.adapter.Drink;
+import com.example.mikky.adapter.DrinkAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DrinkListActivity extends AppCompatActivity {
+
+    private RecyclerView rcvDrink;
+    private DrinkAdapter drinkAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        rcvDrink = findViewById(R.id.rcv_drink);
+        drinkAdapter = new DrinkAdapter(this);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        rcvDrink.setLayoutManager(linearLayoutManager);
+
+        drinkAdapter.setDrinkData(getListDrink());
+
+    }
+
+    //GetData API Function//
+    private List<Drink> getListDrink() {
+        List<Drink> list = new ArrayList<Drink>();
+//        list.add(new Drink(0, "Tra Sua 1", "MT", String.valueOf(R.drawable.ts1), "description", 5.5F));
+        return list;
+    }
+}
