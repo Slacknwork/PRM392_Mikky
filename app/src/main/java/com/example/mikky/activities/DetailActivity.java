7fa97e0f5504bdc5da3ac50752731c08c0293317
An <tannbn01@gmail.com>
2023-02-20 14:27:52 +0700

    GetApi

--- app/src/main/java/com/example/mikky/activities/DetailActivity.java
+++ app/src/main/java/com/example/mikky/activities/DetailActivity.java
@@ -1,20 +1,29 @@
 package com.example.mikky.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.mikky.R;
+import com.example.mikky.instances.RetrofitInstance;
+import com.example.mikky.models.Drink;
+import com.example.mikky.services.ApiInterface;
 import com.squareup.picasso.Picasso;
 
 import java.text.DecimalFormat;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DetailActivity extends AppCompatActivity {
     Toolbar toolbarDetail;
@@ -23,32 +32,65 @@ public class DetailActivity extends AppCompatActivity {
     EditText editTxtAmount;
     Button btnMinus, btnPlus, btnAddCart;
 
+    private ApiInterface apiInterface;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_detail);
         mapping();
-        Back();
-        GetDetail();
+//        back();
+        getApi(1);
     }
 
-    private void GetDetail() {
-        int id = 0;
-        String name = "";
-        int cateId =0;
-        String img = "";
-        String description = "";
-        int price = 0;
+    private void getApi(int drinkId){
+        apiInterface = RetrofitInstance.getRetrofit().create(ApiInterface.class);
+        apiInterface.getItem(drinkId).enqueue(new Callback<Drink>() {
+            @Override
+            public void onResponse(Call<Drink> call, Response<Drink> response) {
+                if (response.isSuccessful()){
+                    Drink drink = response.body();
+                    if (drink != null){
+                        Toast.makeText(DetailActivity.this,"Successfully!",Toast.LENGTH_SHORT).show();
+
+                        txtName.setText(drink.getDrinkname());
+                        DecimalFormat df = new DecimalFormat("###,###,###");
+                        txtPrice.setText(df.format((int) drink.getPrice())+"đ");
+                        txtDescription.setText(drink.getDescription());
+                        String img = drink.getImage();
+                        Picasso.get().load(img).placeholder(R.drawable.noimage).error(R.drawable.error).into(imageDetail);
+                    }else{
+                        Toast.makeText(DetailActivity.this,"Fail!",Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(DetailActivity.this,"Connect fail!",Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<Drink> call, Throwable t) {
+                Toast.makeText(DetailActivity.this,t.getMessage(),Toast.LENGTH_SHORT).show();
+                loadDetail();
+            }
+        });
+    }
+
+    private void loadDetail() {
+        int id = 1;
+        String name = "tra sua";
+        int cateId =1;
+        String img = "https://pos.cafeongbau.com:4433/images/list/tra_sua_tran_chau.png";
+        String description = "aaaaaa";
+        int price = 25000;
+        int amount = 1;
 
         txtName.setText(name);
         DecimalFormat df = new DecimalFormat("###,###,###");
         txtPrice.setText(df.format(price)+"đ");
-        editTxtAmount.setText(0);
         txtDescription.setText(description);
-        Picasso.get().load(img).placeholder(R.drawable.noimage).error(R.drawable.error);
+        Picasso.get().load(img).placeholder(R.drawable.noimage).error(R.drawable.error).into(imageDetail);
     }
 
-    private void Back() {
+    private void back() {
         setSupportActionBar(toolbarDetail);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbarDetail.setNavigationOnClickListener(new View.OnClickListener(){
--- app/src/main/java/com/example/mikky/services/ApiInterface.java
+++ app/src/main/java/com/example/mikky/services/ApiInterface.java
@@ -1,16 +1,16 @@
 package com.example.mikky.services;
 
-import com.example.mikky.models.Product;
+import com.example.mikky.models.Drink;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
+import retrofit2.http.Path;
 
 public interface ApiInterface {
     @GET("All")
-    Call<List<Product>> getposts();
-    @GET("")
-    Call<Product> getItem(@Query("Id")int id);
+    Call<List<Drink>> getposts();
+    @GET("Drink/Drid={id}")
+    Call<Drink> getItem(@Path(value = "id", encoded = true)int id);
 }
